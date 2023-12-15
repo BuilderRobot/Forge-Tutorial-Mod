b61a906fa86458e326235235b828b8b0ec571b3d
jake <jake@stellyfamily.com>
2023-12-14 23:08:40 -0800

    startup

--- src/main/java/net/jakestelly/tutorialmod/TutorialMod.java
+++ src/main/java/net/jakestelly/tutorialmod/TutorialMod.java
@@ -20,9 +20,7 @@ import org.slf4j.Logger;
 @Mod(net.jakestelly.tutorialmod.TutorialMod.MOD_ID)
 public class TutorialMod
 {
-    // Define mod id in a common place for everything to reference
     public static final String MOD_ID = "tutorialmod";
-    // Directly reference a slf4j logger
     private static final Logger LOGGER = LogUtils.getLogger();
     public TutorialMod()
     {
